7d47b6864d8e1393cba3bec15a580bdad07dba83
bhaveshv2 <bhaveshv2@users.noreply.github.com>
2020-05-29 17:39:20 +0530

    Add cartitem component

---
+++ src/CartItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class CartItem extends React.Component{
+    render(){
+        return(
+            <div className="cart-item">
+                <div className="left-block">
+                    {/* eslint-disable-next-line */}
+                    <img style={styles.image}/>
+                </div>
+                <div className="right-block">
+                    <div style={{fontSize:25}}>Phone</div>
+                    <div style={{color:'#777'}}>Rs. 999</div>
+                    <div style={{color:'#777'}}>Qty: 1</div>
+                    <div className="cart-item-actions">
+                        {/* buttons */}
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+// style Elements in jsx itself or pass style as a object
+const styles = {
+    image:{
+        height:110,
+        width:110,
+        borderRadius:4,
+        background:'#ccc'
+    }
+}
+
+export default CartItem;
