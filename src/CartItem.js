f2f12b963fb9e4ceaa726f3b0f6e92183e69be47
bhaveshv2 <bhaveshv2@users.noreply.github.com>
2020-05-29 18:03:49 +0530

    Add cartitem state to it

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,20 +1,31 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            price:1099,
+            title:'Apple Phone',
+            qty:5,
+            img:''
+        }
+    }
     render(){
+        const {price,title,qty}=this.state;       //defining variables from above contructor
         return(
             <div className="cart-item">
                 <div className="left-block">
-                    {/* eslint-disable-next-line */}
-                    <img style={styles.image}/>
+                    <img style={styles.image} alt=""/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>Rs. 999</div>
-                    <div style={{color:'#777'}}>Qty: 1</div>
+                    <div style={{fontSize:25}}>{title}</div>          {/*use the variable title or this.state.title */}
+                    <div style={{color:'#777'}}>Rs. {price}</div>
+        <div style={{color:'#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* buttons */}
-
+                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/992/992651.svg"/>
+                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/1828/1828906.svg"/>
+                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1214/1214428.svg"/>
                     </div>
                 </div>
             </div>
